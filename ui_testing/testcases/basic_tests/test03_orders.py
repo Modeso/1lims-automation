ee13faa2739b8b91f9c7affd46dba3206f2304cc
dianaeldemerdash <diana.mohamed@modeso.ch>
2019-05-17 23:46:12 +0200

    change in order003

--- ui_testing/testcases/basic_tests/test03_orders.py
+++ ui_testing/testcases/basic_tests/test03_orders.py
@@ -1,52 +1,46 @@
-from ui_testing.testcses.base_test import Basetest
+from ui_testing.testcases.base_test import BaseTest
+from ui_testing.pages.login_page import Login
+from ui_testing.pages.order_page import Order
 from ui_testing.pages.article_page import Article
 from ui_testing.pages.testplan_page import TestPlan
-from ui_testing.pages.order_page import Order
-from ui_testing.pages.login_page import Login
 from parameterized import parameterized
 import re
 
 
-class OrdersTestCases (Basetest)
-   def setup (self):
-     super().setup()
-     self.login_page = Login()
-     self.article_page = Article()
-     self.testplan_page = TestPlan()
-     self.order_page = Order()
-     self.login_page.login(username=self.base_selenium.username, password=self.base_selenium.password)
-     self.base_selenium.wait_until_page_url_has(text='dashbord')
-     self.article_page.get_article_page()
+class OrdersTestCases(BaseTest):
+   def setUp(self):
+       super().setUp()
+       self.login_page = Login()
+       self.article_page = Article()
+       self.test_plan = TestPlan()
+       self.order_page = Order()
+       self.login_page.login(username=self.base_selenium.username, password=self.base_selenium.password)
+       self.base_selenium.wait_until_page_url_has(text='dashbord')
+       self.order_page.get_order_page()
 
-     @parameterized.expand(['save', 'cancel'])
-     def test003_cancel_button_edit_materail_type(self,save)
-
+   @parameterized.expand(['save', 'cancel'])
+   def test003_cancel_button_edit_material_type(self, save):
          """
-                 New: Article: Save/Cancel button: After I edit no field then press on cancel button,
-                 a pop up will appear that the data will be
-                 
-                 LIMS-4281
-                 :return:
-                 """
+         New: Orders: Cancel Button Approach: In case I update the material type field then press on cancel button
+         message will appear ( data will be lost )
+         LIMS-4281
+         :return:
+         """
+         self.order_page.get_random_order()
+         order_url = self.base_selenium.get_url()
+         self.order_page.sleep_tiny()
+         current_material_type = self.order_page.get_material_type()
+         self.order_page.set_material_type(random=True)
+         new_material_type = self.order_page.get_material_type()
+         if 'save' == save:
+             self.order_page.save()
+         else:
+             self.order_page.cancel(force=True)
 
-          self.article_page.get_random_article()
-          article_url = self.base_selenium.get_url
-          self.article_page.sleep_tiny()
-          current_material_type = self.article_page.get_material_type()
-          self.article_page.set_material_type(random=true)
-          new_material_type = self.article_page.get_material_type()
+         self.base_selenium.get(url=order_url, sleep=5)
 
-             if'save' == save:
-                 self.article_page.save()
-             else:
-                 self.article_page.cancel(force=True)
-
-              self.base_selenium.get(url=article_url, sleep=5)
-
-          if 'save' == save:
-              self.assertEqual(new_materail_type, self.article_page.get_material_type())
-          else
-              self.assertEqual(current_materail_type, self.article_page.get_material_type())
-
-
+         if 'save' == save:
+             self.assertEqual(new_material_type, self.order_page.get_material_type())
+         else:
+             self.assertEqual(current_material_type, self.order_page.get_material_type())
 
