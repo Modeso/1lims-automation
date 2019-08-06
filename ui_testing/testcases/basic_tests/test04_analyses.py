b55ceefd8d7dcf4fd831103a91033d89384ddafc
mazenmmbadr <mazen.mohamed@modeso.ch>
2019-08-06 07:21:32 +0200

    creating analysis page

---
+++ ui_testing/testcases/basic_tests/test04_analyses.py
@@ -0,0 +1,15 @@
+import re
+from unittest import skip
+from parameterized import parameterized
+from ui_testing.testcases.base_test import BaseTest
+from random import randint
+import time
+
+
+class AnalysesTestCases(BaseTest):
+    def setUp(self):
+        super().setUp()
+        self.login_page.login(
+            username=self.base_selenium.username, password=self.base_selenium.password)
+        self.base_selenium.wait_until_page_url_has(text='dashboard')
+        self.analyses_page.get_analyses_page()
