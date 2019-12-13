d011ad7d7446cfef45bcd1b18a51a87718d7080a
Ahmed Adel <ahmed.adel@modeso.ch>
2019-12-13 14:34:12 +0200

    refactoring

--- ui_testing/testcases/basic_tests/test05_testplans.py
+++ ui_testing/testcases/basic_tests/test05_testplans.py
@@ -8,8 +8,7 @@ import random
 class TestPlansTestCases(BaseTest):
     def setUp(self):
         super().setUp()
-        self.login_page.login(
-            username=self.base_selenium.username, password=self.base_selenium.password)
+        self.login_page.login(username=self.base_selenium.username, password=self.base_selenium.password)
         self.base_selenium.wait_until_page_url_has(text='dashboard')
         self.base_selenium.LOGGER.info('Getting the testplans page')
         self.test_plan.get_test_plans_page()
@@ -24,10 +23,7 @@ class TestPlansTestCases(BaseTest):
         # click on overview from create page
         self.test_plan.click_create_test_plan_button()  # click on create
         self.test_plan.click_overview_link()  # click on overview button
-        # check if redirect to the right page
-        current_page_name = self.test_plan.get_current_page_name()
-        self.assertEqual(current_page_name, 'testPlans')
-        self.test_plan.info('Overview visited from test plan create')
+        current_page_name_from_create = self.test_plan.get_current_page_name() # get current page name
 
         # click on overview from edit page
         test_plan_row = self.test_plan.get_random_table_row('test_plans:test_plans_table') # get random row
@@ -35,8 +31,11 @@ class TestPlansTestCases(BaseTest):
         test_plan_name = test_plan_row_data['Test Plan Name'] # get its name
         self.test_plan.get_test_plan_edit_page(test_plan_name) # open its edit page
         self.test_plan.click_overview_link(with_popup=False)  # click on overview button
+        current_page_name_from_edit = self.test_plan.get_current_page_name() # get current page name
+
         # check if redirect to the right page
-        current_page_name = self.test_plan.get_current_page_name()
-        self.assertEqual(current_page_name, 'testPlans')
         self.test_plan.info('Overview visited from test plan edit')
+        self.assertEqual(current_page_name_from_create, 'testPlans')
+        self.test_plan.info('Overview visited from test plan create')
+        self.assertEqual(current_page_name_from_edit, 'testPlans')
         return
