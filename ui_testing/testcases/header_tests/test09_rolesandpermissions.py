0429c9ba00cba282e179fe5f3684a12eb52983e8
dianaeldemerdash <diana.mohamed@modeso.ch>
2020-02-04 23:41:02 +0200

    fix

--- ui_testing/testcases/header_tests/test09_rolesandpermissions.py
+++ ui_testing/testcases/header_tests/test09_rolesandpermissions.py
@@ -476,11 +476,11 @@ class LoginRandomUser(BaseTest):
 
     def setUp(self):
         super().setUp()
-        random_user_name = self.generate_random_string()
+        self.random_user_name = self.generate_random_string()
         random_user_email = self.base_page.generate_random_email()
         random_user_password = self.generate_random_string()
-        self.users_api.create_new_user(random_user_name, random_user_email, random_user_password)
-        self.login_page.login(username=random_user_name, password=random_user_password)
+        self.users_api.create_new_user(self.random_user_name, random_user_email, random_user_password)
+        self.login_page.login(username=self.random_user_name, password=random_user_password)
         self.base_selenium.wait_until_page_url_has(text='dashboard')
         self.header_page.click_on_header_button()
 
@@ -498,17 +498,13 @@ class LoginRandomUser(BaseTest):
         self.base_selenium.click(element='roles_and_permissions:checked_role_changed_by')
         self.base_selenium.click(element='roles_and_permissions:apply_btn')
 
-        last_row = self.header_page.get_last_role_row()
-        row_data = self.base_selenium.get_row_cells_dict_related_to_header(row=last_row)
-        role_changed_by = row_data['Changed By']
 
         self.base_selenium.click(element='general:menu_filter_view')
         self.header_page.filter_user_drop_down(filter_name='roles_and_permissions:filter_changed_by',
-                                               filter_text=role_changed_by)
+                                               filter_text=self.random_user_name)
 
         roles_result = self.header_page.get_table_rows_data()
-        self.assertIn(row_data['Changed By'], roles_result[0])
-        self.base_selenium.click(element='roles_and_permissions:reset_btn')
+        self.assertIn(self.random_user_name, roles_result[0])
 
 
 
