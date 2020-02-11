5196cce062713eead7dfb6cf41612d596827fca5
ghada-37 <grizk37@gmail.com>
2020-02-11 12:03:28 +0200

    only download 10 users

--- ui_testing/testcases/header_tests/test08_usermanagement.py
+++ ui_testing/testcases/header_tests/test08_usermanagement.py
@@ -75,9 +75,10 @@ class HeaderTestCases(BaseTest):
     #@skip('https://modeso.atlassian.net/browse/LIMS-6563')
     def test004_download_user_sheet(self):
         """
-        User management: Make sure you can export all the data in the active table & it should display in the same order
+        User management: Make sure you can export all the data in
+        the active table & it should display in the same order
+
         LIMS-6101
-        :return:
         """
         self.base_selenium.click(element='header:user_management_button')
         self.base_selenium.LOGGER.info(' * Download XSLX sheet')
