0a27bb0197cd6bf862d6e0950f8df335ed3a898a
dianaeldemerdash <48788927+dianaeldemerdash@users.noreply.github.com>
2020-02-26 16:54:30 +0200

    Update end_points.py

--- api_testing/end_points.py
+++ api_testing/end_points.py
@@ -35,7 +35,6 @@ end_points = {
     'orders_api': {
         'list_all_orders': '/api/orders',
         'get_order_by_id': '/api/orderInformation/get/orders/byId/',
-        'get_order_by_orderId': '/api/orderInformation/get/orders/byId/',
         'create_new_order': '/api/orders',
         'get_auto_generated_number': '/api/orders/auto/generatedId',
         'archive_main_order': '/api/orderInformation/',
