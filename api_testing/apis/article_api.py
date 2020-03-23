72caddc1ce9f29a1bf57dbfe6da463fdf8cb4689
0xIslamTaha <islamtaha2012@gmail.com>
2020-03-23 02:53:55 +0100

    ArticleAPI updates with decoration

--- api_testing/apis/article_api.py
+++ api_testing/apis/article_api.py
@@ -1,6 +1,9 @@
 from api_testing.apis.base_api import BaseAPI
+from api_testing.apis.base_api import api_factory
 
-class ArticleAPIRequests(BaseAPI):
+
+class ArticleAPIFactory(BaseAPI):
+    @api_factory('get')
     def get_all_articles(self, **kwargs):
         api = '{}{}'.format(self.url, self.END_POINTS['article_api']['list_all_articles'])
         _payload = {"sort_value": "number",
@@ -9,65 +12,43 @@ class ArticleAPIRequests(BaseAPI):
                     "sort_order": "DESC",
                     "filter": "{}",
                     "deleted": "0"}
-        return api, _payload
+        return api, _payload, kwargs
 
-
-    def get_article_form_data(self, id=1):
+    @api_factory('get')
+    def get_article_form_data(self, id=1, **kwargs):
         api = '{}{}{}'.format(self.url, self.END_POINTS['article_api']['form_data'], str(id))
-        self.info('GET : {}'.format(api))
-        response_json = self.session.get(api, params='', headers=self.headers, verify=False).json()
-        self.info('Status code: {}'.format(response_json['status']))
-        return response_json
+        _payload = {}
+        return api, _payload, kwargs
 
-    def archive_articles(self, ids=['1']):
+    @api_factory('put')
+    def archive_articles(self, ids=['1'], **kwargs):
         api = '{}{}{}/archive'.format(self.url, self.END_POINTS['article_api']['archive_articles'], ','.join(ids))
-        self.info('PUT : {}'.format(api))
-        response = self.session.put(api, params='', headers=self.headers, verify=False)
-        self.info('Status code: {}'.format(response.status_code))
-        data = response.json()
-        if data['status'] == 1 and data['message'] == 'delete_success':
-            return True
-        else:
-            return False
+        _payload = {}
+        return api, _payload, kwargs
 
-    def restore_articles(self, ids=['1']):
+    @api_factory('put')
+    def restore_articles(self, ids=['1'], **kwargs):
         api = '{}{}{}/restore'.format(self.url, self.END_POINTS['article_api']['restore_articles'], ','.join(ids))
-        self.info('PUT : {}'.format(api))
-        response = self.session.put(api, params='', headers=self.headers, verify=False)
-        self.info('Status code: {}'.format(response.status_code))
-        data = response.json()
-        if data['status'] == 1 and data['message'] == 'restore_success':
-            return True
-        else:
-            return False
+        _payload = {}
+        return api, _payload, kwargs
 
-    def delete_archived_article(self, id=1):
+    @api_factory('delete')
+    def delete_archived_article(self, id=1, **kwargs):
         api = '{}{}{}'.format(self.url, self.END_POINTS['article_api']['delete_article'], str(id))
-        self.info('DELETE : {}'.format(api))
-        response = self.session.delete(api, params='', headers=self.headers, verify=False)
-        self.info('Status code: {}'.format(response.status_code))
-        data = response.json()
-        if data['status'] == 1 and data['message'] == 'hard_delete_success':
-            return True
-        else:
-            return False
+        _payload = {}
+        return api, _payload, kwargs
 
-    def delete_active_article(self, id=1):
-        if self.archive_articles(ids=[str(id)]):
-            if self.delete_archived_article(id=id):
-                return True
-            else:
-                self.restore_articles(ids=[str(id)])
-                return False
-        else:
-            return False
-
+<<<<<<< HEAD
+=======
+    @api_factory('post')
+>>>>>>> ArticleAPI updates with decoration
     def create_article(self, **kwargs):
         """
         Create an article.
         """
         api = '{}{}'.format(self.url, self.END_POINTS['article_api']['create_article'])
         _payload = {
+<<<<<<< HEAD
             "selectedArticles": [],
             "selectedArticlesNos": [],
             "No": self.generate_random_number(),
@@ -75,6 +56,13 @@ class ArticleAPIRequests(BaseAPI):
             "materialType": {
                     "id": 1,
                     "text": "Raw Material"
+=======
+            "No": self.generate_random_number(),
+            "name": self.generate_random_string(),
+            "materialType": {
+                "id": 1,
+                "text": "Raw Material"
+>>>>>>> ArticleAPI updates with decoration
             },
             "selectedMaterialType": [
                 {
@@ -85,30 +73,32 @@ class ArticleAPIRequests(BaseAPI):
             "materialTypeId": 1,
             "dynamicFieldsValues": []
         }
+        return api, _payload, kwargs
 
-        payload = self.update_payload(_payload, **kwargs)
-        self.info('POST : {}'.format(api))
-        response = self.session.post(api, json=payload, params='', headers=self.headers, verify=False).json()
-        self.info('Status code: {}'.format(response['status']))
-        if response['status'] == 1:
-            return response['article'], payload
-        else:
-            return response['message']
-
-    def list_articles_by_materialtype(self, materialtype_id=1, name='', is_archived=0):
+    @api_factory('get')
+    def list_articles_by_materialtype(self, materialtype_id=1, name='', is_archived=0, **kwargs):
         api = '{}{}{}/{}?name={}'.format(self.url, self.END_POINTS['article_api']['list_articles_by_materialtype'],
                                          materialtype_id, is_archived, name)
-        self.info('GET : {}'.format(api))
-        response = self.session.get(api, params='', headers=self.headers, verify=False)
-        self.info('Status code: {}'.format(response.status_code))
-        data = response.json()
-        if data['status'] == 1:
-            return data['articles']
-        return []
+        _payload = {}
+        return api, _payload, kwargs
+
+
+class ArticleAPI(ArticleAPIFactory):
+    def delete_active_article(self, id=1):
+        response = self.archive_articles(ids=[str(id)])
+        if response['status'] == 1 and response['message'] == 'delete_success':
+            delete_response = self.delete_archived_article(id=id)
+            if delete_response['status'] == 1 and delete_response['message'] == 'hard_delete_success':
+                return True
+            else:
+                self.restore_articles(ids=[str(id)])
+                return False
+        else:
+            return False
 
     def get_active_articles_with_material_type(self):
         data = {}
-        articles = self.get_all_articles().json()['articles']
+        articles = self.get_all_articles()[0]['articles']
         for article in articles:
             material_type = article['materialType']
             if material_type not in data.keys():
@@ -138,4 +128,3 @@ class ArticleAPIRequests(BaseAPI):
         all_articles = response.json()['articles']
         articles = [article for article in all_articles if len(article['testPlanNames']) >= 1]
         return articles
-class ArticleAPI(ArticleAPIRequests):
