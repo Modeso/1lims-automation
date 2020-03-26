b177c6b59302b5a9651952404a0960d5588a9227
ghada-37 <57328766+ghada-37@users.noreply.github.com>
2020-03-27 01:54:28 +0200

    Update test06_orders.py

--- ui_testing/testcases/basic_tests/test06_orders.py
+++ ui_testing/testcases/basic_tests/test06_orders.py
@@ -2129,7 +2129,7 @@ class OrdersTestCases(BaseTest):
         # change material type
         self.order_page.open_suborder_edit()
         self.order_page.sleep_small()
-        material_type = self.order_page.set_material_type()
+        self.order_page.set_material_type(material_type='Subassembely')
         self.info('Make sure that article and test units are empty')
         self.assertEqual(self.base_selenium.get_value(element='order:article'), None)
         self.assertEqual(self.base_selenium.get_value(element='order:test_unit'), None)
@@ -2149,6 +2149,6 @@ class OrdersTestCases(BaseTest):
             suborder_data = child_data[0]
 
         # check that it exists
-        self.assertEqual(suborder_data['Material Type'], material_type)
+        self.assertEqual(suborder_data['Material Type'], 'Subassembely')
         self.assertEqual(suborder_data['Article Name'], article)
         self.assertEqual(suborder_data['Test Units'], test_unit)
