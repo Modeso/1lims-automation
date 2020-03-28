cc5ead39f3fbc98f5bb4e272dd7bce2801a8e44d
ghada-37 <grizk37@gmail.com>
2020-03-28 23:47:30 +0200

    replace search with filter

--- ui_testing/testcases/basic_tests/test04_testunits.py
+++ ui_testing/testcases/basic_tests/test04_testunits.py
@@ -768,8 +768,10 @@ class TestUnitsTestCases(BaseTest):
         test_unit_name = random_test_unit['Test Unit Name']
         self.info('test unit name : {}'.format(test_unit_name))
         self.test_unit_page.duplicate_test_unit()
-        self.test_unit_page.sleep_small()
-        found_testunit = self.test_unit_page.search(test_unit_name)[0]
+        self.test_unit_page.apply_filter_scenario(
+            filter_element='test_unit:testunit_name', filter_text=test_unit_name, field_type='text')
+
+        found_testunit = self.test_unit_page.result_table()[0]
         found_testunit_data = self.base_selenium.get_row_cells_dict_related_to_header(row=found_testunit)
         data_changed = ['Test Unit No.', 'Changed On', 'Created On', 'Version']
         random_test_unit, found_testunit_data = self.remove_unduplicated_data(
