b6d90b84656e686db2d9ee4fe6679b99ee9434fc
ghada-37 <grizk37@gmail.com>
2020-05-15 00:37:47 +0200

    updated create order usin api

--- api_testing/apis/orders_api.py
+++ api_testing/apis/orders_api.py
@@ -59,10 +59,18 @@ class OrdersAPIFactory(BaseAPI):
         article = testplan['article'][0]
         article_api = ArticleAPI()
         if article == "all":
-            res, _ = article_api.get_all_articles(limit=20)
+            article = article_api.get_article_with_material_type(material_type=material_type)
+            res, _ = article_api.quick_search_article(name=article)
         else:
             res, _ = article_api.quick_search_article(name=article)
-        article_id = res['articles'][0]['id']
+
+        for _article in res['articles']:
+            if _article['materialType'] == material_type:
+                article_id = _article['id']
+                break
+        else:
+            raise Exception('There is no article with name: {} and material: {}'.format(article, material_type))
+
         testunit = random.choice(TestUnitAPI().list_testunit_by_name_and_material_type(
             materialtype_id=material_type_id)[0]['testUnits'])
 
