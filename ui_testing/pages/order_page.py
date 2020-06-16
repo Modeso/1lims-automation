adc3a91037bb783af3ab5b0a221767fadefb8da9
0xIslamTaha <islamtaha2012@gmail.com>
2020-06-17 00:01:28 +0200

    add sleep time

--- ui_testing/pages/order_page.py
+++ ui_testing/pages/order_page.py
@@ -584,6 +584,7 @@ class Order(Orders):
         super().upload_file(file_name, drop_zone_element, remove_current_file)
         if save:
            self.base_selenium.driver.execute_script("document.querySelector('.dz-details').style.opacity = 'initial';")
+           self.sleep_tiny()
            uploaded_file_name = self.base_selenium.find_element(element='general:uploaded_file_name').text
            self.base_selenium.click('order:uploader_close_btn')
            self.save(save_btn='order:save_btn')
