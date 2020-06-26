bbde79d988f9392661e8bb3abe95c6dc0ceaeab1
ghada-37 <grizk37@gmail.com>
2020-06-26 20:26:37 +0200

    fixed first 20

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ export color_prompt=yes
 cd 1lims-automation;
 export PYTHONPATH='$PYTHONPATH:/1lims-automation';
 export LOGURU_LEVEL='DEBUG';
-xvfb-run -a nosetests -vs --nologcapture --tc-file=config.ini ui_testing/testcases/basic_tests/test04_testunits.py:TestUnitsTestCases
+xvfb-run -a nosetests -vs --nologcapture --tc-file=config.ini --with-flaky --force-flaky --max-runs=3 --min-passes=1 ui_testing/testcases/basic_tests/test04_testunits.py:TestUnitsTestCases
