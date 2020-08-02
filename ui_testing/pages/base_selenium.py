1285e5cc6750ee5a0400884e78a28bd41a670941
0xIslamTaha <islamtaha2012@gmail.com>
2020-08-02 19:59:54 +0200

    fixs usermanagment test cases

--- ui_testing/pages/base_selenium.py
+++ ui_testing/pages/base_selenium.py
@@ -91,6 +91,13 @@ class BaseSelenium:
             options.add_argument('--headless')
             options.add_argument('--no-sandbox')
             options.add_argument('--disable-gpu')
+            options.add_experimental_option("prefs", {
+                "download.default_directory": os.path.expanduser("~/Downloads"),
+                "download.prompt_for_download": False,
+                "download.directory_upgrade": True,
+                "safebrowsing_for_trusted_sources_enabled": False,
+                "safebrowsing.enabled": False
+            })
             self.driver = webdriver.Chrome(chrome_options=options)
         else:
             if self.browser == 'chrome':
