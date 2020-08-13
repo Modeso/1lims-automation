869783bf1c5636a026235283b9482e41544f2d4a
0xIslamTaha <islamtaha2012@gmail.com>
2020-08-13 19:06:34 +0200

    - update the CI and the exeuction scripts. - run against the newtesting

--- .github/workflows/execution.sh
+++ .github/workflows/execution.sh
@@ -30,6 +30,7 @@ echo 'RUN_REF: ' $RUN_REF;
 echo 'RUN_ID: ' $RUN_ID;
 echo 'RUN_NUMBER: ' $RUN_NUMBER;
 echo 'WORK_DIR: ' $WORK_DIR;
+EXECUTION_RESULT=()
 
 for TEST_FILE in "${EXECUTION_FILES[@]}"
  do
@@ -37,7 +38,17 @@ for TEST_FILE in "${EXECUTION_FILES[@]}"
    if [[ $ATTR = "not series" ]]
     then
       docker container run -t --shm-size=2g -v $WORK_DIR:/1lims-automation -e "PYTHONPATH='$PYTHONPATH:/1lims-automation" -w /1lims-automation 0xislamtaha/seleniumchromenose:83 bash -c "NODE_TOTAL=$NODE_TOTAL NODE_INDEX=$NODE_INDEX nosetests -vs --nologcapture --with-reportportal --rp-config-file rp.ini --rp-launch-description=$RUN_REF-$RUN_ID-$RUN_NUMBER --rp-logging-level=WARNING --tc-file=config.ini --tc=browser.headless:True --with-flaky --force-flaky --max-runs=3 --min-passes=1 --with-parallel -A '$ATTR' -m '$TEST_REG' $TEST_FILE"
+      EXECUTION_RESULT+=($?)
     else
       docker container run -t --shm-size=2g -v $WORK_DIR:/1lims-automation -e "PYTHONPATH='$PYTHONPATH:/1lims-automation" -w /1lims-automation 0xislamtaha/seleniumchromenose:83 bash -c "nosetests -vs --nologcapture --with-reportportal --rp-config-file rp.ini --rp-launch-description=$RUN_REF-$RUN_ID-$RUN_NUMBER --rp-logging-level=WARNING --tc-file=config.ini --tc=browser.headless:True --with-flaky --force-flaky --max-runs=3 --min-passes=1 -A '$ATTR' -m '$TEST_REG' $TEST_FILE"
+      EXECUTION_RESULT+=($?)
    fi
  done
+
+for exit_code in "${EXECUTION_RESULT[@]}"
+do
+  if [[ $exit_code = 1 ]]
+  then
+    exit 1;
+  fi
+done
