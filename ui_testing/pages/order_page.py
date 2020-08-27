521f95e079e1672736ad55da49d47baf866ecd54
yara-hossam <yarahossam98@gmail.com>
2020-08-27 19:33:03 +0200

    LIMS-4249: Orders: table/create: Create 5 suborders with same test units ( single select ) and make sure 5 analysis records created successfully according to that.

--- ui_testing/pages/order_page.py
+++ ui_testing/pages/order_page.py
@@ -328,7 +328,7 @@ class Order(Orders):
         self.base_selenium.click(element='order:suborder_list')
 
     def create_new_suborder(self, material_type='', article_name='', test_plan='', test_unit='',
-                            add_new_suborder_btn='order:add_new_item'):
+                            add_new_suborder_btn='order:add_new_item',with_testplan=True):
         # self.get_suborder_table()
         rows_before = self.base_selenium.get_table_rows(element='order:suborder_table')
         self.info('Add new suborder.')
@@ -346,9 +346,10 @@ class Order(Orders):
         else:
             self.set_article(article_name)
         self.sleep_tiny()
-        self.info('Set test plan : {}'.format(test_plan))
-        self.set_test_plan(test_plan)
-        self.sleep_tiny()
+        if with_testplan:
+            self.info('Set test plan : {}'.format(test_plan))
+            self.set_test_plan(test_plan)
+            self.sleep_tiny()
         self.set_test_unit(test_unit)
         self.sleep_tiny()
         return self.get_suborder_data()
--- ui_testing/testcases/basic_tests/test006_orders.py
+++ ui_testing/testcases/basic_tests/test006_orders.py
@@ -1,43 +1,43 @@
-# import re
-# from unittest import skip
-# from parameterized import parameterized
-# from ui_testing.testcases.base_test import BaseTest
-# from ui_testing.pages.order_page import Order
-# from ui_testing.pages.orders_page import Orders
-# from api_testing.apis.orders_api import OrdersAPI
-# from ui_testing.pages.analysis_page import AllAnalysesPage
-# from api_testing.apis.article_api import ArticleAPI
-# from api_testing.apis.test_unit_api import TestUnitAPI
-# from ui_testing.pages.analysis_page import SingleAnalysisPage
-# from api_testing.apis.contacts_api import ContactsAPI
-# from api_testing.apis.test_plan_api import TestPlanAPI
-# from ui_testing.pages.testplan_page import TstPlan
-# from api_testing.apis.general_utilities_api import GeneralUtilitiesAPI
-# from ui_testing.pages.contacts_page import Contacts
-# from random import randint
-# import random
-#
-#
-# class OrdersTestCases(BaseTest):
-#     def setUp(self):
-#         super().setUp()
-#         self.order_page = Order()
-#         self.orders_api = OrdersAPI()
-#         self.testplan_page = TstPlan()
-#         self.orders_page = Orders()
-#         self.analyses_page = AllAnalysesPage()
-#         self.article_api = ArticleAPI()
-#         self.test_unit_api = TestUnitAPI()
-#         self.contacts_api = ContactsAPI()
-#         self.single_analysis_page = SingleAnalysisPage()
-#         self.test_plan_api = TestPlanAPI()
-#         self.contacts_api = ContactsAPI()
-#         self.general_utilities_api = GeneralUtilitiesAPI()
-#         self.contacts_page = Contacts()
-#         self.set_authorization(auth=self.contacts_api.AUTHORIZATION_RESPONSE)
-#         self.order_page.get_orders_page()
-#         self.orders_api.set_configuration()
-#
+import re
+from unittest import skip
+from parameterized import parameterized
+from ui_testing.testcases.base_test import BaseTest
+from ui_testing.pages.order_page import Order
+from ui_testing.pages.orders_page import Orders
+from api_testing.apis.orders_api import OrdersAPI
+from ui_testing.pages.analysis_page import AllAnalysesPage
+from api_testing.apis.article_api import ArticleAPI
+from api_testing.apis.test_unit_api import TestUnitAPI
+from ui_testing.pages.analysis_page import SingleAnalysisPage
+from api_testing.apis.contacts_api import ContactsAPI
+from api_testing.apis.test_plan_api import TestPlanAPI
+from ui_testing.pages.testplan_page import TstPlan
+from api_testing.apis.general_utilities_api import GeneralUtilitiesAPI
+from ui_testing.pages.contacts_page import Contacts
+from random import randint
+import random
+
+
+class OrdersTestCases(BaseTest):
+    def setUp(self):
+        super().setUp()
+        self.order_page = Order()
+        self.orders_api = OrdersAPI()
+        self.testplan_page = TstPlan()
+        self.orders_page = Orders()
+        self.analyses_page = AllAnalysesPage()
+        self.article_api = ArticleAPI()
+        self.test_unit_api = TestUnitAPI()
+        self.contacts_api = ContactsAPI()
+        self.single_analysis_page = SingleAnalysisPage()
+        self.test_plan_api = TestPlanAPI()
+        self.contacts_api = ContactsAPI()
+        self.general_utilities_api = GeneralUtilitiesAPI()
+        self.contacts_page = Contacts()
+        self.set_authorization(auth=self.contacts_api.AUTHORIZATION_RESPONSE)
+        self.order_page.get_orders_page()
+        self.orders_api.set_configuration()
+
 #     @parameterized.expand(['save_btn', 'cancel'])
 #     def test001_edit_order_number_with_save_cancel_btn(self, save):
 #         """
@@ -2296,3 +2296,43 @@
 #                                   f"{str(fixed_sheet_row_data)} : {str(formatted_orders[index])}")
 #             for item in formatted_orders[index]:
 #                 self.assertIn(item, fixed_sheet_row_data)
+
+    def test071_create_suborders_same_testunit(self):
+        """
+        Create 5 suborders with same test units ( single select ) and make sure 5 analysis
+        records created successfully according to that.
+
+        LIMS-4249
+        """
+        response, payload = self.orders_api.create_new_order()
+        self.info('Asserting api success')
+        self.assertEqual(response['status'], 1)
+        order_id = response['order']['mainOrderId']
+        order_no = payload[0]['orderNo']
+        testunit_name = payload[0]['selectedTestUnits'][0]['name']
+        material_type = payload[0]['materialType']['text']
+        self.order_page.get_order_edit_page_by_id(id=order_id)
+        self.order_page.sleep_tiny()
+        for i in range(5):
+            suborders = self.order_page.create_new_suborder(material_type=material_type, test_unit=testunit_name,
+                                                            article_name=' ', with_testplan=False)
+
+        self.order_page.save(save_btn='order:save_btn')
+
+        suborders = self.orders_api.get_suborder_by_order_id(id=order_id)
+        analysis_numbers = [suborder['analysis'][0] for suborder in suborders[0]['orders']]
+        self.info('Asserting there are 5 suborders analysis triggered')
+        self.assertEqual(len(analysis_numbers), 6)
+        self.info('checking testunit for each suborder ')
+        self.order_page.get_orders_page()
+        self.order_page.navigate_to_analysis_tab()
+        self.analyses_page.open_filter_menu()
+        for analysis in analysis_numbers:
+            self.analyses_page.filter_by(
+                filter_element='analysis_page:analysis_no_filter', filter_text=analysis, field_type='text')
+            self.analyses_page.filter_apply()
+            analysis_data = self.analyses_page.get_child_table_data(index=0)
+            self.info('asserting testunit for suborder with analysis number {} is {}, main order testunit is {}'
+                      .format(analysis, analysis_data[0]['Test Unit'], testunit_name))
+            self.orders_page.open_child_table(source=self.analyses_page.result_table()[0])
+            self.assertEqual(analysis_data[0]['Test Unit'], testunit_name)
