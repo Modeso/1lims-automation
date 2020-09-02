df444e056b87834760d20b21aae6f12212390a33
yara-hossam <yarahossam98@gmail.com>
2020-09-02 03:37:46 +0200

    LIMS-4354: Order: Add Same test units in different test plan

--- ui_testing/testcases/basic_tests/test006_orders.py
+++ ui_testing/testcases/basic_tests/test006_orders.py
@@ -2298,13 +2298,16 @@ class OrdersTestCases(BaseTest):
 #                 self.assertIn(item, fixed_sheet_row_data)
 
     def test072_same_testunits_in_different_testplans(self):
+        """
+        Order: Add Same test units in different test plan
+        LIMS-4354
+        """
         formated_testunit, formatted_article, formatted_material, material_type_id =self.test_plan_api.create_random_data_for_testplan()
-        testplan1=self.test_plan_api.create_completed_testplan_random_data(formated_testunit=formated_testunit,formatted_material=formatted_material,formatted_article=formatted_article,material_type_id=material_type_id)
-        testplan1_name=testplan1['testPlan']['text']
-        testplan2=self.test_plan_api.create_completed_testplan_random_data(formated_testunit=formated_testunit,formatted_material=formatted_material,formatted_article=formatted_article,material_type_id=material_type_id)
+        testplan1 = self.test_plan_api.create_completed_testplan_random_data(formated_testunit=formated_testunit, formatted_material=formatted_material,formatted_article=formatted_article,material_type_id=material_type_id)
+        testplan1_name = testplan1['testPlan']['text']
+        testplan2 = self.test_plan_api.create_completed_testplan_random_data(formated_testunit=formated_testunit, formatted_material=formatted_material,formatted_article=formatted_article,material_type_id=material_type_id)
         testplan2_name = testplan2['testPlan']['text']
-        testplans = testplan1_name + ', ' + testplan2_name
-        self.order_page.create_new_order(material_type=formatted_material['text'],article=formatted_article['text'],test_plans=[testplan1_name,testplan2_name],with_testunits=False)
+        self.order_page.create_new_order(material_type=formatted_material['text'], article=formatted_article['text'], test_plans=[testplan1_name,testplan2_name],with_testunits=False)
         self.order_page.sleep_tiny()
         order_id = self.order_page.get_order_id()
         suborders = self.orders_api.get_suborder_by_order_id(id=order_id)
@@ -2317,33 +2320,28 @@ class OrdersTestCases(BaseTest):
         self.order_page.get_orders_page()
         self.order_page.navigate_to_analysis_tab()
         self.analyses_page.apply_filter_scenario(filter_element='analysis_page:analysis_no_filter', filter_text=analysis_number, field_type='text')
-        status=self.analyses_page.get_the_latest_row_data()['Status']
+        status = self.analyses_page.get_the_latest_row_data()['Status']
         self.info('Asserting status of analysis is open')
         self.assertEqual(status,'Open')
         self.info('Asserting correct testplans selected')
         self.assertEqual(self.analyses_page.get_the_latest_row_data()['Test Plans'], testplans)
         analysis_data = self.analyses_page.get_child_table_data(index=0)
         self.info('Asserting 2 child records; one for each test plan')
-        self.assertEqual(len(analysis_data),2)
+        self.assertEqual(len(analysis_data), 2)
         self.orders_page.open_child_table(source=self.analyses_page.result_table()[0])
         for i in range(2):
            self.info('asserting testunit for testplan {} is {} = selected testunit {}'
-                       .format(i+1,analysis_data[i]['Test Unit'], formated_testunit['name']))
+                       .format(i+1, analysis_data[i]['Test Unit'], formated_testunit['name']))
            self.assertEqual(analysis_data[i]['Test Unit'], formated_testunit['name'])
-
-        # --EDITING part DELETE A TEST PLAN
 
         self.orders_page.get_order_edit_page_by_id(order_id)
         self.info('Delete one of the testplans from the order ')
         self.order_page.sleep_tiny()
-        self.info('click on first row and update it')
+        self.info('click on first row and remove a testplan')
         self.order_page.open_suborder_edit()
-        #import ipdb; ipdb.set_trace()
-        #remove first testplan and set to second one only
         self.base_selenium.clear_items_in_drop_down(element='order:test_plan', confirm_popup=True, one_item_only=True)
         self.order_page.save(save_btn='order:save')
         self.order_page.get_orders_page()
-        #self.order_page.navigate_to_analysis_tab()
         self.order_page.sleep_tiny()
         self.analyses_page.apply_filter_scenario(filter_element='analysis_page:analysis_no_filter',
                                                  filter_text=analysis_number, field_type='text')
